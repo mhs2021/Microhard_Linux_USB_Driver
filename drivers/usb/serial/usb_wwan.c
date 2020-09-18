78a9e46dd621ff7c0f8ed908ab34bc1fc17ac360
mhs2021 <71459958+mhs2021@users.noreply.github.com>
2020-09-17 18:20:57 -0600

    Update usb_wwan.c

--- drivers/usb/serial/usb_wwan.c
+++ drivers/usb/serial/usb_wwan.c
@@ -513,6 +513,14 @@ static struct urb *usb_wwan_setup_urb(struct usb_serial_port *port,
 	}
 	#endif
 
+	#if 1 //Added by Microhard for zero packet
+	if (dir == USB_DIR_OUT) {
+		struct usb_device_descriptor *desc = &serial->dev->descriptor;
+	if (desc->idVendor == cpu_to_le16(0x1F94))
+		urb->transfer_flags |= URB_ZERO_PACKET;
+	}
+	#endif
+	
 	return urb;
 }
 
